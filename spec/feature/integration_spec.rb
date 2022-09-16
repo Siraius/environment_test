aacb4bf4b1fce1ff68c6d253f9fbdc2b68332bfe
Kelton Chesshire <mortimore316@gmail.com>
2022-09-15 20:46:26 -0500

    Started Book Collection assignment - Unit Tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,11 +2,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'valid title' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+  end
+
+  scenario 'valid author' do
+    visit new_book_path
+    fill_in 'Author', with: 'J K Rowling'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('J K Rowling')
+  end
+
+  scenario 'valid price' do
+    visit new_book_path
+    fill_in 'Price', with: '9.99'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('9.99')
+  end
+
+  scenario 'valid date' do
+    visit new_book_path
+    fill_in 'Date', with: '2022-10-15'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('2022-10-15')
   end
 end
