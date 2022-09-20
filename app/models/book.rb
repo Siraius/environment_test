42c931b1e1ae8f1c4438f3f1351b8f07497f659b
Kelton Chesshire <kclittlered12@gmail.com>
2022-09-19 20:15:07 -0500

    Finished Book Collection pt 2

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,5 @@
 class Book < ApplicationRecord
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :price, presence: true
-    validates :date, presence: true
+    has_many :users, through: :user_books
+    has_many :user_books
+    validates :title, :author, :price, :date, presence: true
 end
